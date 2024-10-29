c840fad6f50da81ef5fd151568c071cfdaa0b6af
Alexander Tarba <sachatarba@rambler.ru>
2024-10-29 19:04:18 +0300

    added chat client

--- pkg/chatgpt/client.go
+++ pkg/chatgpt/client.go
@@ -2,28 +2,103 @@ package chatgpt
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/ayush6624/go-chatgpt"
+	"github.com/go-resty/resty/v2"
+)
+
+// type ChatGPTClient struct {
+// 	client *chatgpt.Client
+// }
+
+// func NewChatGPTClient(apiKey string) (*ChatGPTClient, error) {
+// 	client, err := chatgpt.NewClient(apiKey)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return &ChatGPTClient{
+// 		client: client,
+// 	}, nil
+// }
+
+// func (c *ChatGPTClient) PostPrompt(ctx context.Context, prompt string) (string, error) {
+// 	resp, err := c.client.Send(ctx, &chatgpt.ChatCompletionRequest{
+// 		Model: chatgpt.ChatGPTModel("gpt-4o-mini"),
+// 		Messages: []chatgpt.ChatMessage{
+// 			{
+// 				Role:    chatgpt.ChatGPTModelRoleSystem,
+// 				Content: prompt,
+// 			},
+// 		},
+// 	})
+// 	if err != nil {
+// 		return "", fmt.Errorf("can't send message to chatgpt: %w", err)
+// 	}
+
+// 	return resp.Object, nil
+// }
+
+// package main
+
+const (
+	url = "https://api.openai.com/v1/chat/completions"
 )
 
 type ChatGPTClient struct {
-	// apiKey string
-	client *chatgpt.Client
+	client *resty.Client
+	apiKey string
 }
 
-func NewChatGPTClient(apiKey string) (*ChatGPTClient, error) {
-	client, err := chatgpt.NewClient(apiKey)
-	if err != nil {
-		return nil, err
-	}
-	
+func NewChatGPTClient(apiKey string) *ChatGPTClient {
 	return &ChatGPTClient{
-		client: client,
-	}, nil
+		client: resty.New(),
+		apiKey: apiKey,
+	}
+}
+
+type Request struct {
+	Model       string           `json:"model"`
+	Messages    []MessageRequest `json:"messages"`
+	Temperature float64          `json:"temperature"`
+}
+
+type MessageRequest struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Response struct {
+	Choices []struct {
+		Message struct {
+			Content string `json:"content"`
+		} `json:"message"`
+	} `json:"choices"`
 }
 
 func (c *ChatGPTClient) PostPrompt(ctx context.Context, prompt string) (string, error) {
-	resp, err := c.client.SimpleSend(ctx, prompt)
-	
-	return resp.Object, err
+	req := Request{
+		Model:       "gpt-4o-mini",
+		Messages:    []MessageRequest{{Role: "user", Content: prompt}},
+		Temperature: 0.7,
+	}
+
+	var resp Response
+	res, err := c.client.R().
+		SetContext(ctx).
+		SetHeader("Content-Type", "application/json").
+		SetAuthToken("Bearer " + c.apiKey).
+		SetBody(req).
+		SetResult(&resp).
+		Post(url)
+
+	if err != nil {
+		return "", fmt.Errorf("%s:%w", res.Body(), err)
+	}
+
+	if len(resp.Choices) > 0 {
+		return resp.Choices[0].Message.Content, nil
+	}
+
+	return "", fmt.Errorf("invalid response from API: %s", res.Body())
 }
